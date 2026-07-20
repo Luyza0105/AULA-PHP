829372152d0db3954e4d2dcf1c2a2f6475dd8aad
Luyza0105 <santosluyza667@gmail.com>
2026-07-20 10:23:50 -0300

    Add files via upload

---
+++ AULA-PHP/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AULA-PHP</title>
+</head>
+<body>
+    <?php
+       $nome = "João";
+
+       $nome = "Maria ";
+
+       const NOME_EMPRESA = 'SIMONETTI';
+       echo "$nome <br> " . NOME_EMPRESA;
+
+       $teclado = 150.50;
+       $mouse = 70.50;
+       $frete = 20.50;
+       $disponivel = true;
+
+       $array = [NOME_EMPRESA,$teclado,
+       $mousse, $frete];
+
+       echo 'olá $nome';
+       echo "<br> olá $nome <br>";
+
+       echo" $nome trabalha na empresa"
+       NOME_EMPRESA . "<br>";
+    
+       
+    ?>
+</body>
+</html>
